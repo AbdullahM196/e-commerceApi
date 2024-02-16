976e90e255688f7217cc46092d2b791760433fa4
AbdullahM196 <abdullah.mahmoud.f196@gmail.com>
2024-02-16 18:16:38 +0200

    updates

--- Routes/subCategoryRoute.js
+++ Routes/subCategoryRoute.js
@@ -6,24 +6,11 @@ const {
 } = require("../Controllers/subCategoryControllers");
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../Middlewares/auth");
 const isAdmin = require("../Middlewares/Admin");
 const { upload } = require("../Middlewares/uploadImage");
 
-router.post(
-  "/add",
-  authMiddleware,
-  isAdmin,
-  upload.single("image"),
-  addSubCategory
-);
+router.post("/add", isAdmin, upload.single("image"), addSubCategory);
 router.get("/", getAllSubCategory);
 router.get("/:id", getSubCategory);
-router.patch(
-  "/update/:id",
-  authMiddleware,
-  isAdmin,
-  upload.single("image"),
-  updateSubCategory
-);
+router.patch("/update/:id", isAdmin, upload.single("image"), updateSubCategory);
 module.exports = router;
